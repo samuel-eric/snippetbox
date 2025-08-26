73e01979dd0deee4e8a2fcff6b826daa6710cd0a
samuel-eric <samueltan086@gmail.com>
2025-08-26 11:07:19 +0700

    add testing for creating snippet endpoint

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -189,3 +189,34 @@ func TestUserSignup(t *testing.T) {
 		})
 	}
 }
+
+func TestSnippetCreate(t *testing.T) {
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	t.Run("Unauthenticated", func(t *testing.T) {
+		ts.resetClientCookieJar(t)
+
+		res := ts.get(t, "/snippet/create")
+		assert.Equal(t, res.status, http.StatusSeeOther)
+		assert.Equal(t, res.headers.Get("Location"), "/user/login")
+	})
+
+	t.Run("Authenticated", func(t *testing.T) {
+		ts.resetClientCookieJar(t)
+
+		res := ts.get(t, "/user/login")
+		csrfToken := extractCSRFToken(t, res.body)
+
+		form := url.Values{}
+		form.Add("email", "alice@example.com")
+		form.Add("password", "pa$$word")
+		form.Add("csrf_token", csrfToken)
+		ts.postForm(t, "/user/login", form)
+
+		res = ts.get(t, "/snippet/create")
+		assert.Equal(t, res.status, http.StatusOK)
+		assert.StringContains(t, res.body, `<form action="/snippet/create" method="post">`)
+	})
+}
